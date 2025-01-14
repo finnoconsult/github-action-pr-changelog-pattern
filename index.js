c8db17726faeabd732edc09053fd0273d9de973b
pitiboy <peter.horvath@finnoconsult.at>
2025-01-14 09:45:07 +0100

    feat: support extraction from pr title

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { extactMessages, messageRegex } = require('./extactMessages');
 try {
 
   const pr = JSON.parse(process.env.PR);
-  messages = pr.body.split('\n');
+  messages = [pr.title, ...pr.body.split('\n')];
 
   const currentVersion = core.getInput('version');
   const messagePattern = core.getInput('message-pattern') || 'https://[\wéáőúíóüö/\-\.]+';
